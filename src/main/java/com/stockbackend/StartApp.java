e72c7b4c7126167f66d0038cbda9f3f9be62b040
ShiXQ01 <shixq01@catl.com>
2024-04-02 11:21:21 +0800

    start app

---
+++ src/main/java/com/stockbackend/StartApp.java
@@ -0,0 +1,21 @@
+package com.stockbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class StartApp extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+        return app.sources(StartApp.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class, args);
+    }
+
+}
