ad8144895d7364810533be46c5de5729daef778b
UGhost <1129202430@qq.com>
2024-08-12 21:39:40 +0800

    modify dbconfig

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -4,6 +4,7 @@ const dbConfig = () => {
   const envFile = process.env.NODE_ENV === "prod" ? ".env.prod" : ".env.dev";
   dotenv.config({ path: envFile });
   const { host, user, password, database, charset } = process.env;  
+  global.logger.info(host);
   return { host, user, password, database, charset } 
 };
 
