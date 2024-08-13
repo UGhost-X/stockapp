cfdde35960d889e1d08d889390b1719d6f8b43c4
UGhost <1129202430@qq.com>
2024-08-13 17:18:04 +0800

    fix pm2 env variable bug

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,11 +2,10 @@ const dotenv = require("dotenv");
 const logger = require("./logconfig");
 
 const dbConfig = () => {
-  // const envFile = process.env.NODE_ENV === "prod" ? ".env.prod" : ".env.dev";
-  logger.info(process.env.NODE_ENV)
-  dotenv.config({ path: '.env.prod' });
+  const envFile = process.env.NODE_ENV.trim() === "prod" ? ".env.prod" : ".env.dev";
+  dotenv.config({ path: envFile });
   const { host, user, password, database, charset } = process.env;
-  return { host, user, password, database, charset };
+  return { host, user, password, database, charset};
 };
 
 module.exports = dbConfig();
--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -2,11 +2,13 @@ module.exports = {
     apps : [
         {
           name: "stockappbackend",
-          script: "./index.js",
+          script: "./src/index.js",
           watch: true,
+          env: {
+            NODE_ENV: 'dev'
+          },
           env_prod: {
-            "PORT": 3000,
-            "NODE_ENV": "prod",
+            NODE_ENV: 'prod'
           }
         }
     ]
