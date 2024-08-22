0b70d80034cc3e6f6026659e3044be37b0f6f805
UGhost <1129202430@qq.com>
2024-08-23 01:23:40 +0800

    modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ global.syncDailyTradeInfoEmailContent = "";
 
 exports.syncDailyStockTradeDataSchedular = async () => {
   logger.info("syncDailyStockTradeDataSchedular had registed")
-  cron.schedule("21 1 * * *", async () => {
+  cron.schedule("0 18 * * *", async () => {
     logger.info("Get All Stocks Data Task Starting....");
     await stockTask.syncDailyStockTradeDataTask();
   });
