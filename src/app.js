02de411428b201af6c8044bfec14804e8a73719f
UGhost <1129202430@qq.com>
2024-08-31 18:14:44 +0800

    modify appjs

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ exports.syncDailyStockTradeDataSchedular = async () => {
 
 exports.calcHistoryMinCloseSchedular = async ()=>{
   logger.info("calcHistoryMinCloseSchedular had registed")
-  cron.schedule("11 19 * * *", async () => {
+  cron.schedule("30 18 * * *", async () => {
     logger.info("Calc History Min Close Task Starting....");
     await stockTask.calcHistoryDailyMinCloseTask();
   });
