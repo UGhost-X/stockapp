e7fcc7591cb1a887c9f509dc3916703ddff7c67a
UGhost <1129202430@qq.com>
2024-09-18 18:18:10 +0800

    modified:   src/ecosystem.config.js

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -10,9 +10,9 @@ module.exports = {
           env_prod: {
             NODE_ENV: 'prod'
           },
-          // restart_delay: 5000, // 重启延迟时间，单位毫秒
-          // max_restarts: 5 // 允许的最大重启次数
-          autorestart: false
+          restart_delay: 5000, // 重启延迟时间，单位毫秒
+          max_restarts: 5, // 允许的最大重启次数
+          autorestart: true
         }
     ]
   }
